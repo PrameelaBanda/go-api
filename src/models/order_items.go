421cd7b40c9b6918dd3a621c8b5a9840bdc03036
PrameelaBanda <39265777+PrameelaBanda@users.noreply.github.com>
2020-07-14 14:17:16 +0530

    updated json syntax

--- src/models/order_items.go
+++ src/models/order_items.go
@@ -1,9 +1,9 @@
 package models
 
 type OrderItems struct {
-    ID         int
-    ORDER_ID   int
-    PRICE_PER_UNIT        float32
-    QUANTITY   int
-    PRODUCT    string
+    ID         int  `json:"ID"`
+    ORDER_ID   int `json:"ORDER_ID"`
+    PRICE_PER_UNIT        float32  `json:"PRICE_PER_UNIT"`
+    QUANTITY   int  `json:"QUANTITY"`
+    PRODUCT    string  `json:"PRODUCT"`
 }
