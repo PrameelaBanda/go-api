3980e27662aea34148b66e11081e5e3dc0aee990
PrameelaBanda <39265777+PrameelaBanda@users.noreply.github.com>
2020-07-14 14:17:45 +0530

    updated json syntax

--- src/models/deliveries.go
+++ src/models/deliveries.go
@@ -1,7 +1,7 @@
 package models
 
 type Deliveries struct {
-   ID                 int
-   ORDER_ITEM         int
-   DELIVERED_QUANTITY int
+   ID                 int  `json:"ID"`
+   ORDER_ITEM         int  `json:"ORDER_ITEM"`
+   DELIVERED_QUANTITY int  `json:"DELIVERED_QUANTITY"`
 }
