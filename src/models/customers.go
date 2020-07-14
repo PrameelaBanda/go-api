8a44ba4d5eac3f31ecee6159d886430c05161535
PrameelaBanda <39265777+PrameelaBanda@users.noreply.github.com>
2020-07-14 14:18:28 +0530

    updated json syntax

--- src/models/customers.go
+++ src/models/customers.go
@@ -1,10 +1,10 @@
 package models
 
 type Customers struct {
-    USER_ID string
-    LOGIN string
-    PASSWORD string
-    NAME string
-    COMPANY_ID int
-    CREDIT_CARDS []string
+    USER_ID string `json:"USER_ID"`
+    LOGIN string `json:"LOGIN"`
+    PASSWORD string `json:"PASSWORD"`
+    NAME string `json:"NAME"`
+    COMPANY_ID int `json:"COMPANY_ID"`
+    CREDIT_CARDS []string `json:"CREDIT_CARDS"`
 }
