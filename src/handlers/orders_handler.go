a1898ee2a90dd419c3e8328dd10cdc3436889a5e
PrameelaBanda <39265777+PrameelaBanda@users.noreply.github.com>
2020-07-14 14:19:43 +0530

    post data implementation

--- src/handlers/orders_handler.go
+++ src/handlers/orders_handler.go
@@ -2,14 +2,18 @@ package handlers
 
 import (
 	"encoding/json"
+	"fmt"
 	"go-api/src/repository"
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"io"
+	"log"
+    "encoding/csv"
 )
 
 func HandleListOrders(r *repository.Repository) gin.HandlerFunc {
 	fn := func(c *gin.Context) {
-		podList, err := r.ListOrders()
+		ordersList, err := r.ListOrders()
 		if err != nil {
 			if dbErr, ok := err.(repository.Dberror); ok {
 				c.AbortWithStatusJSON(dbErr.Code, err)
@@ -19,7 +23,7 @@ func HandleListOrders(r *repository.Repository) gin.HandlerFunc {
 			return
 		}
 
-		b, err := json.MarshalIndent(podList, "", " ")
+		b, err := json.MarshalIndent(ordersList, "", " ")
 		if err != nil {
 			if dbErr, ok := err.(repository.Dberror); ok {
 				c.AbortWithStatusJSON(dbErr.Code, err)
@@ -61,4 +65,58 @@ func HandleGetOrder(r *repository.Repository) gin.HandlerFunc {
 	}
 
 	return fn
+}
+
+func HandlePostData(r *repository.Repository) gin.HandlerFunc {
+	fn := func(c *gin.Context) {
+
+		var result [][]string
+		var err error
+		fileName := c.Param("fileName")
+		resp := csv.NewReader(c.Request.Body)
+		for {
+			record, err := resp.Read()
+			if err == io.EOF {
+				break
+			}
+			if err != nil {
+				return
+			}
+			result = append(result, record)
+		}
+		switch fileName {
+		case "orders" : err = PostOrderData(result, r)
+		case "customers" : err = PostCustomerData(result, r)
+		case "deliveries" : err = PostDeliveriesData(result, r)
+		case "orderItems" : err = PostOrderItemsData(result, r)
+		case "customerCompanies" : err = PostCustomerCompanies(result, r)
+		}
+		if err != nil {
+			if dbErr, ok := err.(repository.Dberror); ok {
+				c.AbortWithStatusJSON(dbErr.Code, err)
+				return
+			}
+			c.AbortWithStatusJSON(http.StatusInternalServerError, err)
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{
+			"message": fmt.Sprintf("Data loaded successfully"),
+		})
+	}
+
+	return fn
+}
+
+func DecodeFileToString(result []string) ([]string) {
+	var row []string
+	b, err := json.Marshal(result)
+	if err != nil {
+		log.Fatal("\nERROR while marshaling the data ", err)
+	}
+	err = json.Unmarshal(b, &row)
+	if err != nil {
+		log.Fatal("\nERROR while unmarshaling the data ", err)
+	}
+	return row
 }
